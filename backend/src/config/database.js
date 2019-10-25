c73c19550dd03620aed97929f170a28204a20594
Diego Rodrigues <diegurm@hotmail.com>
2019-10-24 23:19:04 -0300

    :sparkles: Configurado Multer para upload de arquivo e adicionado avatar ao usuario

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: '123456789',
+  password: 'root$1234',
   database: 'gobarber',
   define: {
     timestamps: true,
