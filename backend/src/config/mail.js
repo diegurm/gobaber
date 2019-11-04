6d53e0908c524162807b23e937792f48cf3d16a3
Diego Rodrigues <diegurm@hotmail.com>
2019-11-03 23:41:54 -0300

    :sparkles: Criado configuração de envio de email

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '462de1678c907e',
+    pass: '939c8f18191f80',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
